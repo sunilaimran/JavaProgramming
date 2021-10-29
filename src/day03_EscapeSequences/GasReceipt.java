8ca221af6e8050f2d5675305a0f0ccc1199fad83
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-10-29 15:24:28 -0400

    day03: Escape Sequences

--- src/day03_EscapeSequences/GasReceipt.java
+++ src/day03_EscapeSequences/GasReceipt.java
@@ -1,7 +1,7 @@
 package day03_EscapeSequences;
 
 public class GasReceipt {
-    
+
     public static void main(String[] args) {
 
         System.out.println();
@@ -13,6 +13,8 @@ public class GasReceipt {
         System.out.println("Price/gallon: $ 2.089");
         System.out.println();
         System.out.println("Fuel total:   $ 24.734");
+
+
 
     }
 
