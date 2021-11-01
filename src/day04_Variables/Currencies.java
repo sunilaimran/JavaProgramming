430bb62432f4aa0e947ca2a4cf0278df61bf38ee
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-01 13:24:54 -0400

    day04: Variables

---
+++ src/day04_Variables/Currencies.java
@@ -0,0 +1,17 @@
+package day04_Variables;
+
+public class Currencies {
+
+    public static void main(String[] args) {
+
+        int dollar = 1000;
+
+        double lira = dollar * 9.53 ;
+        double euro = dollar * 0.86 ;
+        //jpy, pound, peso, cad,  riyal, rub ...
+
+
+
+    }
+
+}
