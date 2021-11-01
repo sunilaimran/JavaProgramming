0aba3a1d6fb3f7b47b1c8ea09913a1c7944febb0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-01 15:22:18 -0400

    day04: Variables

--- src/day04_Variables/GallonsToLitters.java
+++ src/day04_Variables/GallonsToLitters.java
@@ -3,6 +3,11 @@ package day04_Variables;
 public class GallonsToLitters {
 
     public static void main(String[] args) {
+        int gallons = 1000;
+
+        double litters = gallons * 3.79 ;
+
+        System.out.println("litters = " + litters);
 
 
 
