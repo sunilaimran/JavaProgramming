b2233c0cb67a7b43971d0548ddcf916fcd970599
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-01 11:02:15 -0400

    day04

---
+++ src/day04_Variables/PrimitivesIntro.java
@@ -0,0 +1,24 @@
+package day04_Variables;
+
+public class PrimitivesIntro {
+
+    public static void main(String[] args) {
+
+        // age: 38 years old
+        byte age = 38;
+
+        // weight: 160 pounds
+       //  byte weight = 160;  // 160 is out of byte' range
+       //  byte num = -129; // -129 is out of byte' range
+        short weight = 160; // 160 is within the range of short
+
+        //salary: 100000 $
+       // short salary = 100000; // 100000 is out of short' range
+        int salary = 100000; // int is the preferred data type for integer numbers
+
+
+
+    }
+
+
+}
