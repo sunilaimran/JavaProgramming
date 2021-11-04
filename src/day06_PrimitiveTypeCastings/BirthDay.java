018807c4ccc152d9e7f01ce41686d640559c1dc5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-04 10:43:23 -0400

    day06: Primitive Type Castings

--- src/day06_PrimitiveTypeCastings/BirthDay.java
+++ src/day06_PrimitiveTypeCastings/BirthDay.java
@@ -2,6 +2,31 @@ package day06_PrimitiveTypeCastings;
 
 public class BirthDay {
 
+    public static void main(String[] args) {
+        String name = "Aaron";
+        int birthDay = 25;
+        String month = "April";
+        int birthYear = 1995;
+
+        //John was born on month/day/year.
+        System.out.println(name + " was born on " + month + "/" +birthDay +"/"+birthYear );
+
+        System.out.println("-----------------------------------------------");
+
+        // My favorite book is "bookName"
+        String bookName = "The Rich Dad and The Poor Dad";
+
+        System.out.println("My favorite book is " +  "\"" +bookName + "\"");
+        /*
+        \n
+        \t
+        \"
+        \\
+
+         */
+
+
+    }
 
 }
 
