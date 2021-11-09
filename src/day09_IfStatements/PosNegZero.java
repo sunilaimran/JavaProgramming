09c85591b737a6eece8bfad51c4b2f1aa7d2f2b7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-09 12:50:26 -0500

    day09: If Statements

--- src/day09_IfStatements/PosNegZero.java
+++ src/day09_IfStatements/PosNegZero.java
@@ -20,7 +20,31 @@ public class PosNegZero {
 
         System.out.println("--------------------------------");
 
+        boolean positive = n > 0;
+        boolean negative = n < 0;
 
+        if(positive){
+            System.out.println("Positive");
+        }else if(negative){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
+        }
+
+
+        System.out.println("---------------------------------");
+
+        if(positive){
+            System.out.println("Positive");
+        }
+
+        if(negative){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
+        }
+
+    // if & else statement we can NEVER apply for the tasks that requires more than two condition!!
 
 
     }
