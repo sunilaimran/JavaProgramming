bae8bdd9877c2ea6d282164799d80a3ef20fcd95
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-09 12:19:31 -0500

    day09: If Statements

---
+++ src/day09_IfStatements/PosNegZero.java
@@ -0,0 +1,28 @@
+package day09_IfStatements;
+
+public class PosNegZero {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+
+        if(n > 0) {
+            System.out.println("Positive");
+        }
+
+        if(n < 0) {
+            System.out.println("Negative");
+        }
+
+        if(n ==0) {
+            System.out.println("Zero");
+        }
+
+        System.out.println("--------------------------------");
+
+
+
+
+    }
+
+}
