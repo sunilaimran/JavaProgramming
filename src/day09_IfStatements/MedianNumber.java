430cfb208cd8294d2f9be8acf132a78b3d029c68
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-09 11:03:49 -0500

    day08: If Statements

--- src/day09_IfStatements/MedianNumber.java
+++ src/day09_IfStatements/MedianNumber.java
@@ -3,12 +3,17 @@ package day09_IfStatements;
 public class MedianNumber {
 
     public static void main(String[] args) {
-        int a = 10,
-                b = 20,
-                c = 30;
+        int a = 20,
+                b = 50,
+                c = 10;
 
         //               a= 15, b= 10, c = 20    a = 15, c = 10, b = 20
         boolean aIsMedian = (a > b  && a < c) || (a > c && a < b);
+            /*
+            in order for a to be the median number:
+                    1. if c is the maximum number & b is the minimum number, then a is the median number
+                    2. if b is the maximum number & c is the minimum number, then a is the median number
+             */
 
         //              b=15, a = 20, c = 10,       b= 15,  a = 10, c= 20
         boolean bIsMedian = (b > c && b < a)  || (b > a && b < c);
