bd150659fe6435a2fab5631b14a83f42b7e11b41
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-12 14:39:32 -0500

    day10: Nested If

--- src/day10_NestedIf/DaysInWeek.java
+++ src/day10_NestedIf/DaysInWeek.java
@@ -22,6 +22,12 @@ public class DaysInWeek {
             System.out.println("Sunday");
         }
 
+        System.out.println("------------------------------------------------");
+
+        String result = (n == 1) ? "Monday" :(n == 2)? "Tuesday" :(n == 3)? "Wednesday" :( n == 4)? "Thursday"
+                        :(n == 5)? "Friday" :(n==6)? "Saturday" : "Sunday";
+
+        System.out.println(result);
 
 
     }
