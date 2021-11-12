1fc9cf2459ccc9ebacbe2b54d1853a7ace624392
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-12 10:43:15 -0500

    day10: Nested If

--- src/day10_NestedIf/NestedIfIntro.java
+++ src/day10_NestedIf/NestedIfIntro.java
@@ -41,7 +41,29 @@ public class NestedIfIntro {
 
         System.out.println("-------------------------------------------");
 
-        int number = 5;
+        int number = -1;
+
+        if(number >= 1 && number<= 7){ // if the number is a valid number (1~7)
+
+            if(number == 1){
+                System.out.println("Monday");
+            }else if(number == 2){
+                System.out.println("Tuesday");
+            }else if(number == 3){
+                System.out.println("Wednesday");
+            }else if(number == 4){
+                System.out.println("Thursday");
+            }else if(number == 5){
+                System.out.println("Friday");
+            }else if(number == 6){
+                System.out.println("Saturday");
+            }else{
+                System.out.println("Sunday");
+            }
+
+        }else{ // if the number is not valid
+            System.out.println("Invalid Number");
+        }
 
 
 
