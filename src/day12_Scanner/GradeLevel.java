aac4c331fea4494a62c2e7fb75ba05a062c2d068
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-16 10:27:51 -0500

    day12: Scanner

--- src/day12_Scanner/GradeLevel.java
+++ src/day12_Scanner/GradeLevel.java
@@ -7,6 +7,32 @@ public class GradeLevel {
         String result = "";
 
 
+        switch (number){
+            case 1: case 2: case 3: case 4: case 5:
+                result = "Elementary school";
+
+            case 6: case 7: case 8:
+                result = "Middle school";
+                break;
+
+            case 9: case 10: case 11: case 12:
+                result = "High school";
+                break;
+
+            case 13: case 14: case 15: case 16:
+                result = "College";
+                break;
+
+            case 17: case 18:
+                result = "Grad School";
+                break;
+
+            default: // 1~5
+                result = "Invalid Grade Level";
+        }
+
+
+        /*
         if(number>= 1 && number <= 18){  // 1 ~ 18
 
             switch (number){ // 1 ~ 18:  case: 6~18,  default: 1~5
@@ -33,6 +59,8 @@ public class GradeLevel {
         }else{
            result = "Invalid Grade Level";
         }
+*/
+
 
         System.out.println(result);
 
