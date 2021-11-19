48b3e4722314bb6119994166fa3b023d5bac6755
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-19 14:41:31 -0500

    day14: String

--- src/day14_String/StringMethod2.java
+++ src/day14_String/StringMethod2.java
@@ -23,6 +23,48 @@ public class StringMethod2 {
                sentence2= sentence2.replace("   ", " ");
         System.out.println("sentence2 = " + sentence2);
 
+
+        String s = "Dog Dog Dog Dog Dog Dog";
+        s = s.replace("Dog", "Cat"); //"Cat ...."
+
+        System.out.println("s = " + s);
+
+        String s2 = "C# is fun, C# is cool";
+
+        s2 = s2.replace(" C#", " Java");
+        //s2 = s2.replace("C# is c", "Java is c");
+
+        System.out.println("s2 = " + s2);
+
+        String s3 = "Java";
+
+        s3 = s3.replace("a", "e"); //"Jeve"
+
+        System.out.println(s3);
+
+        System.out.println("--------------------------------------------");
+
+        String result = "Java Java Java";
+
+        //result = result.replace("Java", "Python");
+        result = result.replaceFirst("Java", "Python");
+        System.out.println(result);
+
+        String result2 = "C# is fun, C# is cool";
+
+        result2 =  result2.replaceFirst("C#", "Java");
+
+        System.out.println(result2);
+
+        String result3 = "Java";
+        result3 = result3.replaceFirst("va", "vo"); //"Jova"
+
+        System.out.println(result3);
+
+
+
+
+
     }
 
 }
