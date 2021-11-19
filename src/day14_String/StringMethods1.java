b888d295a07ea5b947de6f21c3f0300fe0f180b0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-19 11:51:02 -0500

    day14: String

--- src/day14_String/StringMethods1.java
+++ src/day14_String/StringMethods1.java
@@ -31,6 +31,38 @@ public class StringMethods1 {
 
         System.out.println("n5 = " + n5);
 
+        System.out.println("-------------------------------");
+
+        String s = "Java Nova Cava Wawa orange";
+     //             012345
+
+        int firstA = s.indexOf("a");
+        int lastA = s.lastIndexOf("a");
+        int secondA = s.indexOf("a ");
+        int thirdA = s.indexOf("a C");
+        // int fourthA = s.indexOf("ava W");
+       // int fourthA = s.lastIndexOf("av");
+        int fourthA = s.indexOf("Ca") + 1;
+       // int fifthA = s.lastIndexOf("va") + 1;
+        int fifthA = s.lastIndexOf("a W") ;
+        int sixthA = s.lastIndexOf("aw");
+        //int seventhA = s.indexOf("a o");
+        int seventhA = s.lastIndexOf("a ");
+
+
+        System.out.println("firstA = " + firstA);
+        System.out.println("lastA = " + lastA);
+        System.out.println("secondA = " + secondA);
+        System.out.println("thirdA = " + thirdA);
+        System.out.println("fourthA = " + fourthA);
+        System.out.println("fifthA = " + fifthA);
+        System.out.println("sixthA = " + sixthA);
+        System.out.println("seventhA = " + seventhA);
+
+
+
+
+
 
 
     }
