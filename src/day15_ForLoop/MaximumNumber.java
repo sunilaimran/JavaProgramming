9fa73d2242527376059cf58b98bec3eea3f62da2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-22 16:00:28 -0500

    day15: For Loop

---
+++ src/day15_ForLoop/MaximumNumber.java
@@ -0,0 +1,17 @@
+package day15_ForLoop;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
+/*
+Write a program that asks the user to enter a number for 5 times.
+return the maximum number
+
+ */
---
+++ src/day15_ForLoop/MinimumNumber.java
@@ -0,0 +1,11 @@
+package day15_ForLoop;
+
+public class MinimumNumber {
+
+}
+
+/*
+Write a program that asks the user to enter a number for 5 times.
+return the maximum number
+
+ */
