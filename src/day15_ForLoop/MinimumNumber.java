7d42504a4fd1e4a9c1245254c0373459b3ef799e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-22 17:01:01 -0500

    day15: For Loop

--- src/day15_ForLoop/MinimumNumber.java
+++ src/day15_ForLoop/MinimumNumber.java
@@ -7,8 +7,22 @@ public class MinimumNumber {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int min= 2147483647; //
+        int min = 2147483647; //any number that user enter will be less than 2147483647
+        // min = -10
 
+        for(int i = 10; i < 15; i++ ){
+
+            System.out.println("Enter a number:");
+            int num = scan.nextInt(); //67, 23, 42, 5, -10
+
+            if(num < min ){
+                min = num;
+            }
+
+        }
+
+
+        System.out.println("min = " + min);
 
 
     }
