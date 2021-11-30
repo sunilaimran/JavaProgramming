411ed041ec60b83daf472b82f06aa08f58258c39
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-30 13:00:11 -0500

    day18: nested loops

--- src/day18_NestedLoop/OddOrEven.java
+++ src/day18_NestedLoop/OddOrEven.java
@@ -1,8 +1,40 @@
 package day18_NestedLoop;
 
+import java.util.Scanner;
+
 public class OddOrEven {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+     while(true) {
+         System.out.println("Enter a number");
+         int num = scan.nextInt();
+
+         if (num % 2 == 0) {
+             System.out.println(num + " is even number");
+         } else {
+             System.out.println(num + " is odd number");
+         }
+
+         System.out.println("Would you like to enter another number?");
+         String a = scan.next().toLowerCase();
+
+         while( !(a.equals("yes") || a.equals("no")) ){ // if the answer is invalid (not yes or no)
+             System.err.println("Please re enter, Would you like to enter another number?");
+             a = scan.next().toLowerCase();
+         }
+
+         if(a.equals("no")){
+             break;
+         }
+
+     }
+
+
+
 
     }
 
@@ -16,6 +48,6 @@ public class OddOrEven {
                 if user enters yes, repeat the previous steps
                 if user enters no, print nothing
 
-                If user enteres invalid answer, repeat the previous steps
+                If user enters invalid answer, repeat the previous steps
 
  */
