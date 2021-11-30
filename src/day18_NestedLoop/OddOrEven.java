654bfafd2c47f95b462a10bae16170236a67626e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-30 12:45:43 -0500

    day18: nested loops

---
+++ src/day18_NestedLoop/OddOrEven.java
@@ -0,0 +1,21 @@
+package day18_NestedLoop;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+2. Write a program that asks user to enter a numnber , and check if the number is odd or even.
+        if it's odd, print "Odd Number"
+        if it's even, print "Even number"
+
+        Ask the user "Would you like to enter another number"
+                if user enters yes, repeat the previous steps
+                if user enters no, print nothing
+
+                If user enteres invalid answer, repeat the previous steps
+
+ */
