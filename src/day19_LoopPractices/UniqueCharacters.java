69c74f836f07453f9c7785fdb38f014dc5f61f93
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-02 10:52:52 -0500

    day19: Loops

---
+++ src/day19_LoopPractices/UniqueCharacters.java
@@ -0,0 +1,20 @@
+package day19_LoopPractices;
+
+public class UniqueCharacters {
+
+    public static void main(String[] args) {
+        String str = "aabccdeef";
+
+
+    }
+
+}
+/*
+2. Write a program that can find the unique characters from a string without using indexOf() and lastIndexOf() methods
+
+    			Ex:
+                        str = "aabccdeef";
+
+                        output:
+                                bdf
+ */
