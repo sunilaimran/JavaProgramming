fdcb4f3b51d6494e5b0be1098440a3807b5b01b5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-02 11:58:12 -0500

    day19: Loops

--- src/day19_LoopPractices/UniqueCharacters.java
+++ src/day19_LoopPractices/UniqueCharacters.java
@@ -7,17 +7,36 @@ public class UniqueCharacters {
         String result = ""; //bdf
 
 
-        char ch = 'a';
-        int count = 0; // represents the frequency of the ch
-        for (int i = 0; i < str.length(); i++) {
-            char each = str.charAt(i); // each character of str
-            if(ch == each){
-                count++;
+        for (int j = 0; j < str.length(); j++) {
+
+            char ch = str.charAt(j); //'A'
+            int count = 0; // represents the frequency of the ch
+
+            for (int i = 0; i < str.length(); i++) { //compares the character that outer loop picked, with each character of the string,
+                char each = str.charAt(i); // each character of str
+                if(ch == each){
+                    count++;
+                }
             }
+
+/*
+            if(count == 1){ // if the frequency of the character is 1, then the character is unique
+                result += ch;
+            }
+ */
+            if(count != 1){
+                continue;
+            }
+
+            result += ch;
+
         }
 
 
-        System.out.println(count);
+        System.out.println(result);
+
+
+
 
     }
 
