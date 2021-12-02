99f9aab0aa248948e0e02555fe315a99cfc2c06e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-02 11:00:00 -0500

    day19: Loops

--- src/day19_LoopPractices/UniqueCharacters.java
+++ src/day19_LoopPractices/UniqueCharacters.java
@@ -4,7 +4,20 @@ public class UniqueCharacters {
 
     public static void main(String[] args) {
         String str = "aabccdeef";
+        String result = ""; //bdf
 
+
+        char ch = 'a';
+        int count = 0; // represents the frequency of the ch
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i); // each character of str
+            if(ch == each){
+                count++;
+            }
+        }
+
+
+        System.out.println(count);
 
     }
 
