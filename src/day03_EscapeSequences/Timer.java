4d296c233553ca504841c7969ad5788e55b0d520
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-03 11:10:59 -0500

    day20: Arrays

--- src/day03_EscapeSequences/Timer.java
+++ src/day03_EscapeSequences/Timer.java
@@ -13,8 +13,8 @@ public class Timer {
 
         for (int i=minutes; i > 0 ;i--) {
             for (int z=59; z >=0 ;z--){
-                System.out.print( "\r"+i+" minutes and "+z+" seconds left");
-                Thread.sleep(10);
+                System.out.print( "\r"+(i-1)+" minutes and "+z+" seconds left");
+                Thread.sleep(1000);
             }
         }
 
