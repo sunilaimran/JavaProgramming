87bc7619973f3d5cbb1d176ed3c18753bfd740d1
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-03 16:03:57 -0500

    day20: Arrays

---
+++ src/day20_Arrays/MinMaxNumbers.java
@@ -0,0 +1,17 @@
+package day20_Arrays;
+
+public class MinMaxNumbers {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+/*
+3. Write a program that asks user to enter a number 10 times:
+			1. store all user entered numbers in an array
+			2. find the max number
+			3. find the min number
+ */
