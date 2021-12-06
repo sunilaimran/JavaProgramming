6ff42ec4c842fea7c457c309cf498dc98d6e75e8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-06 14:33:45 -0500

    day21: For Each Loop

--- src/day21_ForEachLoop/StringMethods.java
+++ src/day21_ForEachLoop/StringMethods.java
@@ -17,6 +17,27 @@ public class StringMethods {
 
         System.out.println("-------------------------------------");
 
+        String sentence = "Wooden Spoon";
+
+        String[] words =  sentence.split(" ");
+
+        System.out.println(Arrays.toString(words));
+
+        System.out.println("------------------------------------");
+
+        String email = "WoodenSpoon@cydeo.com";
+
+        String[] arr = email.split("@");
+
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("------------------------------------------------");
+
+        String s = "Today is nice day. Today is Monday. Today we learn Java";
+
+        String[] sentences =s.split("\\. ");
+
+        System.out.println(Arrays.toString(sentences));
 
 
     }
