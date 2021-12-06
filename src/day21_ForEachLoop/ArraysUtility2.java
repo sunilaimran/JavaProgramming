34f8c1aa9122cf37970291fcbcb316263c0a0b79
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-06 11:35:04 -0500

    day21: For Each Loop

--- src/day21_ForEachLoop/ArraysUtility2.java
+++ src/day21_ForEachLoop/ArraysUtility2.java
@@ -26,6 +26,17 @@ public class ArraysUtility2 {
         System.out.println(Arrays.toString(ch2));
 
 
+        int[] scores = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        //index:         0   1   2   3   4   5   6   7   8  9
+        int[] result = Arrays.copyOfRange(scores, 3, 8);
+
+
+        System.out.println(Arrays.toString(result));
+
+
+        int[] result2 = Arrays.copyOfRange(scores, 5, scores.length);
+        System.out.println(Arrays.toString(result2));
+
 
 
 
