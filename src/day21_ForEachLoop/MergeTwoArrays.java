2c5aca414524a6d8b59b209da33fdaaa41e09d8c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-06 16:05:51 -0500

    day21: For Each Loop

--- src/day21_ForEachLoop/MergeTwoArrays.java
+++ src/day21_ForEachLoop/MergeTwoArrays.java
@@ -25,6 +25,28 @@ public class MergeTwoArrays {
         System.out.println(Arrays.toString(students));
 
 
+        System.out.println("-----------------------------------------------");
+
+        char[] ch1 = {'A', 'B', 'C'};
+        char[] ch2 = {'D', 'E', 'F', 'G', 'H'};
+
+        char[] chars = new char[ch1.length + ch2.length];
+
+        int j = 0;
+
+        for (char ch : ch1) {
+            chars[j]= ch;
+            j++;
+        }
+
+        for (char ch : ch2) {
+            chars[j]= ch;
+            j++;
+        }
+
+        System.out.println(Arrays.toString(chars));
+
+
     }
 
 }
