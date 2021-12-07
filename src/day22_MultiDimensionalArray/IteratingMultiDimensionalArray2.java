a8081e3b97f61d5725afa7185ff90a3092f04681
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-07 12:11:29 -0500

    day22: Multi Dimensional Array

--- src/day22_MultiDimensionalArray/IteratingMultiDimensionalArray2.java
+++ src/day22_MultiDimensionalArray/IteratingMultiDimensionalArray2.java
@@ -18,6 +18,19 @@ public class IteratingMultiDimensionalArray2 {
 
         System.out.println("-------------------------------------------------");
 
+        for (int i = 0; i < arr2D.length; i++) { // i: indexes of each 1D array started from 0
+
+            for (int j = arr2D[i].length - 1; j >= 0; j--) { // j: indexes of each element starting from last index
+                System.out.print( arr2D[i][j]+" " );
+            }
+
+            System.out.println();
+
+        }
+
+        System.out.println("------------------------------------------");
+
+
 
     }
 
@@ -29,10 +42,14 @@ task1:
                 9 10 11 12 13
                 4 5 6 7 8
                 1 2 3
-
 task2:
                 3 2 1
                 8 7 6 5 4
                 13 12 11 10 9
+
+ task3:
+                13 12 11 10 9
+                8 7 6 5 4
+                3 2 1
 
  */
