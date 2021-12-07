af8e87bd0661fdea79a2c5a0394af8aac19b83f0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-07 11:39:14 -0500

    day22: Multi Dimensional Array

--- src/day22_MultiDimensionalArray/MultiDimensionalArrayIntro.java
+++ src/day22_MultiDimensionalArray/MultiDimensionalArrayIntro.java
@@ -31,11 +31,14 @@ public class MultiDimensionalArrayIntro {
         int[][] arr2D = { {1,2,3} , {4,5,6,7}, {8,9,10,11,12}  };
     // index of arrays:       0         1            2
 
+        System.out.println( Arrays.deepToString(arr2D) );
 
         // {4,5,6,7}
+        System.out.println( Arrays.toString(arr2D[1]) );
 
 
         //11
+        System.out.println( arr2D[2][3] );
 
 
 
