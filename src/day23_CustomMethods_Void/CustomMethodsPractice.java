b375a8976befbe8211a7f8ed36c65c41a601006d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-09 12:28:31 -0500

    day23: Custom Methods

--- src/day23_CustomMethods_Void/CustomMethodsPractice.java
+++ src/day23_CustomMethods_Void/CustomMethodsPractice.java
@@ -6,9 +6,13 @@ public class CustomMethodsPractice {
 
         helloWorld5Times();
 
+        System.out.println("------------------------------");
+
+        helloCydeo5Times();
 
 
     }
+
 
     // create a function that can print hello world 5 times ==> helloWorld5Times()
     public static void helloWorld5Times(){
@@ -21,6 +25,24 @@ public class CustomMethodsPractice {
 
 
     // create a function that can print hello Cydeo 5 times  ==> helloCydeo5Times
+    public static void helloCydeo5Times(){
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello Cydeo!");
+        }
+
+    }
+
+
+    // create a function that can print all the even numbers from 1 ~ 10 ==> evenNumbers
+    public static void evenNumbers(){
+
+        for (int i = 2; i < 11 ; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+
 
 
 }
