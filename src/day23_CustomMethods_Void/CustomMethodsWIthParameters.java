1dbcb61182a14776a26cb028eac7776dc486a145
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-09 13:01:48 -0500

    day23: Custom Methods

--- src/day23_CustomMethods_Void/CustomMethodsWIthParameters.java
+++ src/day23_CustomMethods_Void/CustomMethodsWIthParameters.java
@@ -8,6 +8,9 @@ public class CustomMethodsWIthParameters {
 
         oddOrEven(10);
 
+        ageOfPerson(1995);
+
+        printNumbers( 1000, 2000);
 
 
     }
@@ -26,6 +29,20 @@ public class CustomMethodsWIthParameters {
 
 
     // create a function that can display the age of the person
+    public static void ageOfPerson(int birthYear){
+
+        int age = 2021 - birthYear;
+
+        System.out.println("Your age is: "+age);
+
+    }
+
+    //                                                       10       50
+    //create a function that can print all the numbers between X and Y
+    public static void printNumbers(int x, int y){
+
+
+    }
 
 
 }
