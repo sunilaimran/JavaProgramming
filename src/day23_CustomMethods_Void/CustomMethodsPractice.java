c81bf0acc6ab9462181c492c88f7eed480e48ab2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-09 12:08:17 -0500

    day23: Custom Methods

---
+++ src/day23_CustomMethods_Void/CustomMethodsPractice.java
@@ -0,0 +1,26 @@
+package day23_CustomMethods_Void;
+
+public class CustomMethodsPractice {
+
+    public static void main(String[] args) {
+
+        helloWorld5Times();
+
+
+
+    }
+
+    // create a function that can print hello world 5 times ==> helloWorld5Times()
+    public static void helloWorld5Times(){
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+
+    }
+
+
+    // create a function that can print hello Cydeo 5 times  ==> helloCydeo5Times
+
+
+}
