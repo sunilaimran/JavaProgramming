ffc17b213f63e94d17cecb6c44ed3439408598b6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-10 11:50:50 -0500

    day24: Custom Methods - Return Methods

--- src/day24_CustomMethods_Return/BreakfastTasks.java
+++ src/day24_CustomMethods_Return/BreakfastTasks.java
@@ -42,7 +42,24 @@ public class BreakfastTasks {
 
 
     //3. Create a method that can display the name of the month based on the given number to the method
-    //public static void
+    public static void nameOfMonth(int number){
+
+        String name = "";
+
+        if(number >= 1 && number <= 12){
+
+            name = (number==1)?"Jan" :(number==2)?"Feb" :(number==3)?"Mar" :(number==4)?"Apr" :(number==5)?"May"
+                    :(number==6)?"Jun" :(number==7)?"Jul" :(number==8)?"Aug" :(number==9)?"Sep" :(number==10)?"Oct"
+                    :(number==11)?"Nov" : "Dec";
+
+        }else{
+            name = "Invalid";
+        }
+
+        System.out.println("Month name = " + name);
+
+    }
+
 
 
 }
