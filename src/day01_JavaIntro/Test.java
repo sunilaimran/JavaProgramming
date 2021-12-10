81b17c3f8570d4ef86e8478468f1d89a5abc107f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-10 16:59:08 -0500

    day24: Custom Methods - Return Methods

---
+++ src/day01_JavaIntro/Test.java
@@ -0,0 +1,28 @@
+package day01_JavaIntro;
+
+import day24_CustomMethods_Return.ReturnMethodIntro;
+import day24_CustomMethods_Return.ReturnMethodPractice4;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String str = "aaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbb";
+
+        str = ReturnMethodPractice4.removeDuplicates(str);
+
+        System.out.println(str);
+
+
+        String name = "Java Programming";
+
+        String reversedName = ReturnMethodIntro.reverse(name);
+
+        System.out.println(reversedName);
+
+
+    }
+
+
+
+}
