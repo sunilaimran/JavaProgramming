5bdf6975a42ae7cd69db7ce39d00ed0457f30271
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-10 11:39:38 -0500

    day24: Custom Methods - Return Methods

--- src/day24_CustomMethods_Return/BreakfastTasks.java
+++ src/day24_CustomMethods_Return/BreakfastTasks.java
@@ -6,12 +6,26 @@ public class BreakfastTasks {
 
         initials("cydeo", "school"); //
 
+        System.out.println("----------------------------");
+
+        domain("Cydeo.School@amazon.com");
+
+        System.out.println("-----------------------------");
+
+        String[] emails = {"josh@gmail.com", "Jim@yahoo.com", "Elminur@cydeo.com", "Gulsen@gmail.com" };
+
+        for (String email : emails) {
+            domain(email);
+        }
+
+
+
     }
 
     //1. Create a method that can display the initials of the person.    initials(String firstName, String lastName)
-    public static void initials(String firstName, String lastName){
+    public static void initials(String firstName, String lastName) {
 
-        String initial = firstName.charAt(0)+"."+lastName.charAt(0);
+        String initial = firstName.charAt(0) + "." + lastName.charAt(0);
         initial = initial.toUpperCase();
         System.out.println("initial = " + initial);
 
@@ -19,6 +33,12 @@ public class BreakfastTasks {
     }
 
     //2. Create a method that can display the domain of the email.  domain(String email)
+    public static void domain(String email) { // Cydeo.School@gmail.com
+
+        String domain = email.substring(email.indexOf("@") + 1, email.lastIndexOf("."));
+        System.out.println("domain = " + domain);
+
+    }
 
 
 }
