b715e5257c99259967274449ad6ac0938af64dc8
Baros2535 <brsaltn.25@gmail.com>
2021-12-11 19:49:44 +0300

    Day2 Avenger Practice Hour

--- AvengerPracticeHour/Day2_12_11_2021/StringWithoutXFirstAndLast.java
+++ AvengerPracticeHour/Day2_12_11_2021/StringWithoutXFirstAndLast.java
@@ -15,21 +15,21 @@ public class StringWithoutXFirstAndLast {
      */
 
     public static void main(String[] args) {
-        String word="Hellox";
+        String word="xHellox";
         String temp="";
                         //0123
               // input:  "xHix" → Hi
-                         //012
-              //   input :"xHi" → Hi
-                         //0123
-              //   input :"Hxix" → Hxi
-
-
         if (word.charAt(0)=='x'&&word.charAt(word.length()-1)=='x'){
+            /*
             for (int i = 1; i <word.length() -1; i++) {
                 temp+=word.charAt(i);
             }
+
+             */
+            temp=word.substring(1,word.length()-1);
         }else if (word.charAt(0)=='x'){
+            //012
+            //   input :"xHi" → Hi
             for (int i = 1; i <word.length() ; i++) {
                 temp+=word.charAt(i);
             }
