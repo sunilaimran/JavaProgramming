af40071a5b43fd0f535acf5612dc7847529012b9
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-13 15:49:38 -0500

    day25: Custom Methods - Overloading

---
+++ src/utilities/ArraysUtility.java
@@ -0,0 +1,30 @@
+package utilities;
+
+public class ArraysUtility {
+
+    //prints each integer of an integer array in separate lines
+    public static void printEach(int[] array){
+        for (int each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    //prints each double of double array in separate lines
+    public static void printEach(double[] array){
+        for (double each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+
+    public static void printEach(char[] array){
+        for (char each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+
+}
