1699bfde1a581471e01a7c7ad8e05fea29e61fac
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-13 14:26:03 -0500

    day25: Custom Methods - Overloading

--- src/day01_JavaIntro/Test.java
+++ src/day01_JavaIntro/Test.java
@@ -1,24 +1,12 @@
 package day01_JavaIntro;
 
-import day24_CustomMethods_Return.ReturnMethodIntro;
-import day24_CustomMethods_Return.ReturnMethodPractice4;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        String str = "aaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbb";
-
-        str = ReturnMethodPractice4.removeDuplicates(str);
-
-        System.out.println(str);
+        String str = "Wooden Spoon";
 
 
-        String name = "Java Programming";
-
-        String reversedName = ReturnMethodIntro.reverse(name);
-
-        System.out.println(reversedName);
 
 
     }
---
+++ src/utilities/StringUtility.java
@@ -0,0 +1,17 @@
+package utilities;
+
+public class StringUtility {
+
+    //prints each character of the given string
+    public static void printEachChar(String str){
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+
+    }
+
+
+
+
+}
