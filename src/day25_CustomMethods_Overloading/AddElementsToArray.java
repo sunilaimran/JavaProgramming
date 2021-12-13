b2f3820f9c1f728b7a7fea09a148f44b568e8999
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-13 10:55:35 -0500

    day25: Custom Methods - Overloading

--- src/day25_CustomMethods_Overloading/AddElementsToArray.java
+++ src/day25_CustomMethods_Overloading/AddElementsToArray.java
@@ -21,6 +21,17 @@ public class AddElementsToArray {
 
         System.out.println("--------------------------------------");
 
+        String[] names = {"Tatiana", "Oleksandr", "Cassandra", "Ali"};  //"Neira"
+
+         names = addString(names, "Neira");  //{"Tatiana", "Oleksandr", "Cassandra", "Ali", "Neira"}
+
+        names = addString(names, "Igor");
+
+        System.out.println(Arrays.toString(names));
+
+        System.out.println("--------------------------------------");
+
+
 
     }
 
@@ -49,15 +60,31 @@ public class AddElementsToArray {
             result[i++] = each;
         }
 
-        result[i] = element;
-
+        result[result.length-1] = element; // element need to be assigned to the last index
+       // result[i] = element;
+        
         return result;
 
     }
 
 
     //3. create a return method called addString that can add a String after the last index of a String array
+    public static String[] addString(String[] array, String element){
+        String[] result = new String[array.length + 1];
 
+        int i =0;
+        for (String each : array) {
+            result[i++] = each;
+        }
+
+        result[result.length-1] = element; // element need to be assigned to the last index
+        // result[i] = element;
+
+        return result;
+
+    }
+    
+    
 
 }
 /*
