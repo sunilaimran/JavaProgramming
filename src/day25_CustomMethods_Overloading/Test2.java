1f036486ec568ffc918e2d9083a4d58822211ca8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-13 16:09:09 -0500

    day25: Custom Methods - Overloading

--- src/day25_CustomMethods_Overloading/Test2.java
+++ src/day25_CustomMethods_Overloading/Test2.java
@@ -6,9 +6,27 @@ public class Test2 {
 
     public static void main(String[] args) {
 
-        int[] arr1 = {1,2,3,4,5};
+        int[] arr1 = {1, 2, 3, 4, 5};
 
-       // ArraysUtility.
+        ArraysUtility.printEachElement(arr1);
+
+        System.out.println("--------------------------------");
+
+        double[] arr2 = {1.5, 2.5, 3.5, 4.5};
+
+        ArraysUtility.printEachElement(arr2);
+
+        System.out.println("--------------------------------");
+
+        char[] arr3 = {'A', 'B', 'C', 'D'};
+
+        ArraysUtility.printEachElement(arr3);
+
+        System.out.println("--------------------------------");
+
+        String[] names = {"David", "Elvira", "Ali", "Igor"};
+
+        ArraysUtility.printEachElement(names);
 
 
 
