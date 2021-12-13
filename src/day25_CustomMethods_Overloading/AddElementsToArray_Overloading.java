a495e73f6138d025cf37d4877ba3c96a47f01ece
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-13 12:58:09 -0500

    day25: Custom Methods - Overloading

--- src/day25_CustomMethods_Overloading/AddElementsToArray_Overloading.java
+++ src/day25_CustomMethods_Overloading/AddElementsToArray_Overloading.java
@@ -1,10 +1,40 @@
 package day25_CustomMethods_Overloading;
 
+import java.util.Arrays;
+
 public class AddElementsToArray_Overloading {
 
 
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5,6};  //7
+
+        numbers = addElement(numbers, 7);
+
+        char[] chars = {'A', 'B', 'C', 'D'}; //'E'
+
+         chars = addElement(chars, 'E');
+
+        String[] names = {"Ahmet", "Oleksandr", "Sinem", "Cihad"}; // "Layan"
+
+        names = addElement(names, "Layan");
+
+
+        System.out.println("numbers = " + Arrays.toString(numbers) );
+        System.out.println("chars = " + Arrays.toString(chars) );
+        System.out.println("names = " + Arrays.toString(names));
+
+
+
+
+
+    }
+
+
+
+
     //1. create a return method called addInteger that can add an Integer  after the  last index of an integer array
-    public static int[] addInteger(int[] array, int element){
+    public static int[] addElement(int[] array, int element){
         int[] result = new int[array.length + 1];
 
         int i =0;
@@ -19,7 +49,7 @@ public class AddElementsToArray_Overloading {
 
 
     //2. create a return method called addDouble that can add a double after the last index of a double array
-    public static double[] addDouble(double[] array, double element){
+    public static double[] addElement(double[] array, double element){
         double[] result = new double[array.length + 1];
 
         int i =0;
@@ -36,7 +66,7 @@ public class AddElementsToArray_Overloading {
 
 
     //3. create a return method called addString that can add a String after the last index of a String array
-    public static String[] addString(String[] array, String element){
+    public static String[] addElement(String[] array, String element){
         String[] result = new String[array.length + 1];
 
         int i =0;
@@ -53,7 +83,7 @@ public class AddElementsToArray_Overloading {
 
 
     //4. create a return method called addChar that can add a char after last index of a char array
-    public static char[] addChar(char[] array, char element){
+    public static char[] addElement(char[] array, char element){
         char[] result = new char[array.length + 1];
 
         int i =0;
