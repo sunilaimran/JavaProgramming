b05bdedbfdc974e5610d629e00ed8d684870b673
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 10:09:14 -0500

    day26: Custom Methods Practice

---
+++ src/day26_CustomMethodsPractice/FrequencyOfElement.java
@@ -0,0 +1,15 @@
+package day26_CustomMethodsPractice;
+
+public class FrequencyOfElement {
+}
+/*
+1. Create a class named FrequencyOfElement:
+			1.1 Create a method that passes two parameters: an integer array and an Integer element. The method returns the frequency of the given integer element from the integer array
+
+			1.2 Create a method that passes two parameters: a double array and a double element. The method returns the frequency of the given integer from the array
+
+			1.3 Create a method that passes two parameters: a char array and a char element. The method returns the frequency of the given char element from the char array
+
+			1.4 Create a method that passes two parameters: a String array and a String element. The method returns the frequency of the given String element from the String array
+
+ */
