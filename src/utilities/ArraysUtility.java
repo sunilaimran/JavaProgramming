bf209741334b0375e990b548e527c0b26760472d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 12:45:55 -0500

    day26: Custom Methods Practice

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -401,10 +401,74 @@ public class ArraysUtility {
 
 
     // merge the given two arrays and returns the new array
+    public static int[] merge(int[] arr1, int[] arr2){
+        int[] result = {};
+
+        for (int each : arr1) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        for (int each : arr2) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        return result;
+    }
+
+
+    // merge the given two arrays and returns the new array
+    public static double[] merge(double[] arr1, double[] arr2){
+
+        double[] result = {};
+
+        for (double each : arr1) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        for (double each : arr2) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        return result;
+    }
+
+
+    // merge the given two arrays and returns the new array
+    public static char[] merge(char[] arr1, char[] arr2){
+
+        char[] result = {};
+
+        for (char each : arr1) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        for (char each : arr2) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        return result;
+    }
+
+
+    // merge the given two arrays and returns the new array
+    public static String[] merge(String[] arr1, String[] arr2){
+
+        String[] result = {};
+
+        for (String each : arr1) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        for (String each : arr2) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        return result;
+    }
 
 
 
-    //merge
+
     //reverse
     //removeDuplicates
 
