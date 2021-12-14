bc50938a5481171fd7a7713ea15d870c39c65e55
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 12:34:50 -0500

    day26: Custom Methods Practice

--- src/day26_CustomMethodsPractice/MergeTwoArrays2.java
+++ src/day26_CustomMethodsPractice/MergeTwoArrays2.java
@@ -1,8 +1,24 @@
 package day26_CustomMethodsPractice;
 
+
+import utilities.ArraysUtility;
+
 public class MergeTwoArrays2 {
 
-  // public static int[] merge(int[] arr1, int[] arr2){}
+
+   public static int[] merge(int[] arr1, int[] arr2){
+       int[] result = {};
+
+       for (int each : arr1) {
+           result = ArraysUtility.addElement(result, each);
+       }
+
+       for (int each : arr2) {
+           result = ArraysUtility.addElement(result, each);
+       }
+
+       return result;
+   }
 
 
 }
