d4b28ae75489144e87a0a5208eaef5f8b4f43f04
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 10:53:45 -0500

    day26: Custom Methods Practice

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -246,11 +246,74 @@ public class ArraysUtility {
 
 
 
+    // returns the unique elements of the array as a new array
+    public static int[] uniqueElements(int[] array){
+        int[] result = {}; //new int[0]
+
+        for (int each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
+    // returns the unique elements of the array as a new array
+    public static double[] uniqueElements(double[] array){
+        double[] result = {}; //new int[0]
+
+        for (double each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
+    // returns the unique elements of the array as a new array
+    public static char[] uniqueElements(char[] array){
+        char[] result = {}; //new int[0]
+
+        for (char each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
+    // returns the unique elements of the array as a new array
+    public static String[] uniqueElements(String[] array){
+        String[] result = {}; //new int[0]
+
+        for (String each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
 
     //merge
     //removeElements
     //reverse
-    //uniqueElement
     //removeDuplicates
 
 
