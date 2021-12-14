06c19498525ef44e6bdbec76f3d66c3dcdbdbcf4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 11:44:34 -0500

    day26: Custom Methods Practice

--- src/day26_CustomMethodsPractice/RemoveElements1.java
+++ src/day26_CustomMethodsPractice/RemoveElements1.java
@@ -65,6 +65,52 @@ public class RemoveElements1 {
 
 
 
+    //removes the index from the array, returns new array
+    public static char[] removeElement(char[] array, int index){
+
+        if(index < 0 || index > array.length-1){
+            System.err.println("Invalid Index: "+index);
+            System.exit(0);
+        }
+
+        char[] result = new char[array.length -1]; //1,2
+
+        int j = 0;
+        for (int i= 0; i < array.length; i++) {
+            if(i == index){  // if the index of array is matching with the given index
+                continue; // skip
+            }
+            result[j++] = array[i];
+        }
+
+        return result;
+    }
+
+
+
+    //removes the index from the array, returns new array
+    public static String[] removeElement(String[] array, int index){
+
+        if(index < 0 || index > array.length-1){
+            System.err.println("Invalid Index: "+index);
+            System.exit(0);
+        }
+
+        String[] result = new String[array.length -1]; //1,2
+
+        int j = 0;
+        for (int i= 0; i < array.length; i++) {
+            if(i == index){  // if the index of array is matching with the given index
+                continue; // skip
+            }
+            result[j++] = array[i];
+        }
+
+        return result;
+    }
+
+
+
 
 }
 
