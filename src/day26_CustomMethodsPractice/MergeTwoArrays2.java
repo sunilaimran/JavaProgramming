8e6f72b3a171805b9752efe65685042484beaaac
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 12:36:15 -0500

    day26: Custom Methods Practice

--- src/day26_CustomMethodsPractice/MergeTwoArrays2.java
+++ src/day26_CustomMethodsPractice/MergeTwoArrays2.java
@@ -3,7 +3,20 @@ package day26_CustomMethodsPractice;
 
 import utilities.ArraysUtility;
 
+import java.util.Arrays;
+
 public class MergeTwoArrays2 {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,3,4};
+        int[] arr2 = {5,6,7};
+
+        int[] arr3 = merge(arr1, arr2);
+
+        System.out.println(Arrays.toString(arr3));
+
+    }
 
 
    public static int[] merge(int[] arr1, int[] arr2){
