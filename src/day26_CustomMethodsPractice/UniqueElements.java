bc0970d3ce24ff4b3be7fdf0f3fe0929159c75a7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 10:52:37 -0500

    day26: Custom Methods Practice

--- src/day26_CustomMethodsPractice/UniqueElements.java
+++ src/day26_CustomMethodsPractice/UniqueElements.java
@@ -54,4 +54,37 @@ public class UniqueElements {
     }
 
 
+    // returns the unique elements of the array as a new array
+    public static char[] uniqueElements(char[] array){
+        char[] result = {}; //new int[0]
+
+        for (char each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
+    // returns the unique elements of the array as a new array
+    public static String[] uniqueElements(String[] array){
+        String[] result = {}; //new int[0]
+
+        for (String each : array) {
+
+            if( ArraysUtility.frequencyOfElement(array,each) == 1 ){ // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result, each);
+            }
+
+        }
+
+        return result;
+    }
+
+
+
 }
