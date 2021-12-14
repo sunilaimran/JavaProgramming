3df000af1012a85f07b2b80407ba4c892447a482
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-14 11:10:33 -0500

    day26: Custom Methods Practice

---
+++ src/day26_CustomMethodsPractice/RemoveElements1.java
@@ -0,0 +1,48 @@
+package day26_CustomMethodsPractice;
+
+public class RemoveElements1 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //                              {1,2,3,4, 3}   ,    4
+    public static int[] removeElement(int[] array, int index){
+
+        if(index < 0 || index > array.length-1){
+            System.err.println("Invalid Index: "+index);
+            System.exit(0);
+        }
+
+        int[] result = new int[array.length -1]; //1,2
+
+        int j = 0;
+        for (int i= 0; i < array.length; i++) {
+            if(i == index){  // if the index of array is matching with the given index
+                continue; // skip
+            }
+            result[j++] = array[i];
+        }
+
+        return result;
+    }
+
+
+
+}
+
+/*
+2. Create a class named RemoveElements:
+			2.1 Create a method that passes two parametetrs: an integer array and an integer index. the method removes the element at the given index of the array and returns the new array
+					Ex:
+						int[] arr = {10,20,30,40}
+						removeElement(arr, 2) ==> {10, 20, 40}
+
+			2.2 Create a method that passes two parametetrs: a double array and an integer index. the method removes the element at the given index of the array and returns the new array
+
+
+			2.3 Create a method that passes two parametetrs: a char array and an integer index. the method removes the element at the given index of the array and returns the new array
+
+			2.4 Create a method that passes two parametetrs: a String array and an integer index. the method removes the element at the given index of the array and returns the new array
+ */
