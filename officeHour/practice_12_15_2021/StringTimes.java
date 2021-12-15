c683355e3dc6f3e4b80dc389e4ecfd2c811663d3
Aysun120 <aysundamar114@gmail.com>
2021-12-15 13:25:18 -0600

    Office Hours Day 2 notes

---
+++ officeHour/practice_12_15_2021/StringTimes.java
@@ -0,0 +1,15 @@
+package practice_12_15_2021;
+
+public class StringTimes {
+
+    public static void main(String[] args) {
+        /*
+           Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+             input String : Hi  ,  input int : 2
+             output: HiHi
+         */
+
+        
+    }
+}
