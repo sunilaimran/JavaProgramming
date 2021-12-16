b843ab0c935f8d765712dd568be6169de777f432
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 12:55:52 -0500

    day27: Wrapper Classes

--- src/day27_WrapperClasses/WrapperClassMethods.java
+++ src/day27_WrapperClasses/WrapperClassMethods.java
@@ -53,6 +53,47 @@ public class WrapperClassMethods {
         System.out.println("-----------------------------------------------------");
 
 
+        char ch1 = '!';
+
+        //isDigit()
+       boolean r2 = Character.isDigit(ch1);
+
+        //isLetter()
+       boolean r3 = Character.isLetter(ch1);
+
+        // special char
+        boolean r4= !Character.isLetterOrDigit(ch1);
+
+        // upperCase
+        boolean r5 = Character.isUpperCase(ch1);
+
+        //lowerCase
+        boolean r6 = Character.isLowerCase(ch1);
+
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r6);
+
+        System.out.println("----------------------------------");
+
+        String s = "ab1cde2efg3hi4";
+
+        int sum = 0;
+
+        for (char each : s.toCharArray()) {
+
+            if( Character.isDigit(each) ){
+                sum +=  Integer.parseInt(""+each);
+            }
+
+        }
+
+        System.out.println("sum = " + sum);
+
+
+
 
 
 
