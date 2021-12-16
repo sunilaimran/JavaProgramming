99d6fe1b0c3dc8404b8b2624e8ab4a6e1165a485
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 10:55:42 -0500

    day27: Wrapper Classes

--- src/day27_WrapperClasses/RemoveDuplicates.java
+++ src/day27_WrapperClasses/RemoveDuplicates.java
@@ -1,4 +1,35 @@
 package day27_WrapperClasses;
 
+import utilities.ArraysUtility;
+
+import java.util.Arrays;
+
 public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4};
+
+        arr = removeDuplicates(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+    //
+    public static int[] removeDuplicates(int[] array){
+
+        int[] result = {};//{1,
+
+        for (int each : array) {
+            if(!ArraysUtility.contains(result, each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        return result;
+    }
+
+
 }
