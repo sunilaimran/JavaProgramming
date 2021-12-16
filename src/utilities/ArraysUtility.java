012e5eccf3fb54193bb6d1fa9f7702a9fb62883c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 10:43:24 -0500

    day27: Wrapper Classes

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -573,6 +573,70 @@ public class ArraysUtility {
     }
 
 
+    // replaces all the matching old values of the array with the new value
+    public static int[] replaceAll(int[] array, int oldValue, int newValue) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == oldValue) {
+                array[i] = newValue;
+            }
+
+        }
+
+        return array;
+
+    }
+
+
+    // replaces all the matching old values of the array with the new value
+    public static double[] replaceAll(double[] array, double oldValue, double newValue) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == oldValue) {
+                array[i] = newValue;
+            }
+
+        }
+
+        return array;
+
+    }
+
+
+    // replaces all the matching old values of the array with the new value
+    public static char[] replaceAll(char[] array, char oldValue, char newValue) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == oldValue) {
+                array[i] = newValue;
+            }
+
+        }
+
+        return array;
+
+    }
+
+
+    // replaces all the matching old values of the array with the new value
+    public static String[] replaceAll(String[] array, String oldValue, String newValue) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].equals( oldValue) ) {
+                array[i] = newValue;
+            }
+
+        }
+
+        return array;
+
+    }
+
+
 
     //removeDuplicates
 
