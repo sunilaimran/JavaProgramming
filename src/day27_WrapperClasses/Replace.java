fe8f5622b61714153a0db0d93d3a8eb3fda5a7e8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 10:21:13 -0500

    day27: Wrapper Classes

--- src/day27_WrapperClasses/Replace.java
+++ src/day27_WrapperClasses/Replace.java
@@ -11,6 +11,14 @@ public class Replace {
 
         System.out.println(Arrays.toString(arr));
 
+        System.out.println("------------------------------------");
+
+        String[] arr2 = {"Java", "Python", "C++", "Ruby"};
+
+        arr2 = replace(arr2, 2, "C#");
+
+        System.out.println(Arrays.toString(arr2));
+
     }
 
 
@@ -29,6 +37,31 @@ public class Replace {
 
 
     public static double[] replace(double[] array, int index, double newElement){
+
+        if(index <0 || index > array.length-1){
+            System.err.println("Invalid Index: "+index);
+            System.exit(0);
+        }
+
+        array[index] = newElement;
+        return array;
+    }
+
+
+
+    public static char[] replace(char[] array, int index, char newElement){
+
+        if(index <0 || index > array.length-1){
+            System.err.println("Invalid Index: "+index);
+            System.exit(0);
+        }
+
+        array[index] = newElement;
+        return array;
+    }
+
+
+    public static String[] replace(String[] array, int index, String newElement){
 
         if(index <0 || index > array.length-1){
             System.err.println("Invalid Index: "+index);
