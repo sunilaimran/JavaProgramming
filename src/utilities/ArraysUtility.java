1f30f959921204de39ba6f39a6d186165b604168
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 11:08:00 -0500

    day27: Wrapper Classes

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -638,7 +638,65 @@ public class ArraysUtility {
 
 
 
-    //removeDuplicates
+    //removes the duplicates from the given array, returns the new array
+    public static int[] removeDuplicates(int[] array){
+
+        int[] result = {};//{1,
+
+        for (int each : array) {
+            if(!ArraysUtility.contains(result, each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        return result;
+    }
+
+
+    //removes the duplicates from the given array, returns the new array
+    public static double[] removeDuplicates(double[] array){
+
+        double[] result = {};//{1,
+
+        for (double each : array) {
+            if(!ArraysUtility.contains(result, each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        return result;
+    }
+
+
+    //removes the duplicates from the given array, returns the new array
+    public static char[] removeDuplicates(char[] array){
+
+        char[] result = {};//{1,
+
+        for (char each : array) {
+            if(!ArraysUtility.contains(result, each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        return result;
+    }
+
+
+    //removes the duplicates from the given array, returns the new array
+    public static String[] removeDuplicates(String[] array){
+
+        String[] result = {};//{1,
+
+        for (String each : array) {
+            if(!ArraysUtility.contains(result, each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        return result;
+    }
+
 
 
 
