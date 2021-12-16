925a32b2be02d5e0ae456b9c9cf15ce006a4640a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-16 10:39:37 -0500

    day27: Wrapper Classes

--- src/day27_WrapperClasses/ReplaceAll.java
+++ src/day27_WrapperClasses/ReplaceAll.java
@@ -18,25 +18,39 @@ public class ReplaceAll {
         System.out.println(Arrays.toString(arr));
 
 
+    }
+
+
+    // replaces all the matching old values of the array with the new value
+    public static int[] replaceAll(int[] array, int oldValue, int newValue) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == oldValue) {
+                array[i] = newValue;
+            }
+
+        }
+
+        return array;
 
     }
 
 
     // replaces all the matching old values of the array with the new value
-  public static int[] replaceAll(int[] array, int oldValue, int newValue){
+    public static double[] replaceAll(double[] array, double oldValue, double newValue) {
 
-      for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
 
-          if(array[i] == oldValue ){
-              array[i] = newValue;
-          }
+            if (array[i] == oldValue) {
+                array[i] = newValue;
+            }
 
-      }
+        }
 
-      return array;
+        return array;
 
-  }
-
+    }
 
 
 
