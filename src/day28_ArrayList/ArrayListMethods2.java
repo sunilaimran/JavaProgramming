471b7395d8e8d656e192813d32c3285cf1c5e513
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-17 15:14:42 -0500

    day28: ArrayList

--- src/day28_ArrayList/ArrayListMethods2.java
+++ src/day28_ArrayList/ArrayListMethods2.java
@@ -1,4 +1,46 @@
 package day28_ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayListMethods2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(200);
+        list.add(200);
+        list.add(300);
+        list.add(400);
+        list.add(500);
+
+        System.out.println(list);
+        /*
+        int num = 1;
+        list.remove(num);
+*/
+        Integer num = 2000;
+        //list.remove(200);
+        boolean r = list.remove(num);
+
+        System.out.println(list);
+        System.out.println(r);
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println(list.size());
+
+        list.clear();
+
+        System.out.println(list.size());
+
+        System.out.println(list);
+
+
+
+
+    }
+
+
 }
