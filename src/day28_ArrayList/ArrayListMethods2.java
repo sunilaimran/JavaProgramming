9b90a1b077897dbb4e9a069494a242ad33d9fc4f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-17 16:59:51 -0500

    day28: ArrayList

--- src/day28_ArrayList/ArrayListMethods2.java
+++ src/day28_ArrayList/ArrayListMethods2.java
@@ -1,6 +1,7 @@
 package day28_ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListMethods2 {
 
@@ -76,8 +77,19 @@ public class ArrayListMethods2 {
         System.out.println(list1 == list2);
         System.out.println(list1.equals(list2));
 
+        System.out.println("-----------------------------------------");
+        list1.clear();
+       boolean r4 =  list1.isEmpty(); // true
 
-        
+        System.out.println("r4 = " + r4);
+
+        System.out.println("----------------------------------");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+    //Bulk Operation: CollectionType
+        numbers.addAll( Arrays.asList(1,2,3,4,5,6,7) );
+
+        System.out.println(numbers);
 
     }
 
