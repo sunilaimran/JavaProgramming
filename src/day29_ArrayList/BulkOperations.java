f664c5c9f81ff3a244f04cf43e4608f2e5fbe09b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-20 11:31:32 -0500

    day29: ArrayList

--- src/day29_ArrayList/BulkOperations.java
+++ src/day29_ArrayList/BulkOperations.java
@@ -25,7 +25,43 @@ public class BulkOperations {
 
         System.out.println(numbers);
 
+        System.out.println("-------------------------------------------");
 
+        ArrayList<String> jobTitles = new ArrayList<>();
+        jobTitles.addAll( Arrays.asList("QA", "SDET", "Developer", "QA", "SDET", "Scrum Master", "BA", "BA"));
+
+        jobTitles.retainAll( Arrays.asList("QA", "SDET") );
+
+        System.out.println(jobTitles);
+
+        System.out.println("-------------------------------------------");
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.addAll(  Arrays.asList(1,2,3,4,5,6,7,1,2,3,8,9,10) );
+
+        boolean r1 = nums.contains(10);
+
+        boolean r2 = nums.contains(2) && nums.contains(5) && nums.contains(10);
+
+        boolean r3 = nums.containsAll( Arrays.asList(2, 5, 10, 1000) );
+
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+
+
+        System.out.println("-----------------------------------------------");
+
+        String[] names = {"Josh", "Jack", "Daniel", "Shay", "Breanna"};
+
+        ArrayList<String> namesList = new ArrayList<>(Arrays.asList(names));
+
+        /*
+        ArrayList<String> namesList = new ArrayList<>();
+        namesList.addAll( Arrays.asList(names) );
+        */
+
+        System.out.println(namesList);
 
 
 
