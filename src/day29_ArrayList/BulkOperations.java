e69c20dce23814322f45c78562fdac91446bb1a6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-20 11:41:34 -0500

    day29: ArrayList

--- src/day29_ArrayList/BulkOperations.java
+++ src/day29_ArrayList/BulkOperations.java
@@ -63,8 +63,37 @@ public class BulkOperations {
 
         System.out.println(namesList);
 
+        System.out.println("------------------------------------------------------");
 
+        Integer[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        ArrayList<Integer> list2 = new ArrayList<>( Arrays.asList(arr));
+
+        System.out.println(list2);
+
+        System.out.println("-----------------------------------------------------");
+
+        int[] arr2 = {1,2,3,4,5,6,7,8,9,10};
+
+        ArrayList<Integer> list3 = new ArrayList<>( convertArrayToArrayList(arr2) );
+
+        System.out.println("list3 = " + list3);
 
     }
+
+
+    public static ArrayList<Integer>  convertArrayToArrayList(int[] array){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int each : array) {
+            list.add(each);
+        }
+
+        return list;
+    }
+
+
+
+
 
 }
