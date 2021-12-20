a788404cd6132569e896d35b60840a8c262b1abb
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-20 15:52:50 -0500

    day29: ArrayList

--- src/day29_ArrayList/ArrayListPractice2.java
+++ src/day29_ArrayList/ArrayListPractice2.java
@@ -17,6 +17,16 @@ public class ArrayListPractice2 {
 
         String[] names = {"Mary", "Monica", "Mehray", "Musti", "Sumeyra", "Hasan", "Beril"};
 
+        ArrayList<String> list = new ArrayList<>( Arrays.asList(names) );
+
+        list.removeIf( p -> p.charAt(0) == 'M' );
+
+        names = list.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(names));
+
+
+
 
 
     }
