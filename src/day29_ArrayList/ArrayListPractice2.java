57b381620ef920be58468e3df5fd98e33861cab3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-20 15:46:23 -0500

    day29: ArrayList

---
+++ src/day29_ArrayList/ArrayListPractice2.java
@@ -0,0 +1,25 @@
+package day29_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListPractice2 {
+
+    public static void main(String[] args) {
+        ArrayList<String> employees =new ArrayList<>();
+        employees.addAll(Arrays.asList("Ali", "David", "Ahmed", "Jimmy", "Daniel", "Aaron", "Ahmed", "David", "Shay"));
+
+        employees.retainAll( Arrays.asList("Ahmed", "David") );
+
+        System.out.println(employees);
+
+        System.out.println("--------------------------------------------------------------");
+
+        String[] names = {"Mary", "Monica", "Mehray", "Musti", "Sumeyra", "Hasan", "Beril"};
+
+
+
+    }
+
+
+}
