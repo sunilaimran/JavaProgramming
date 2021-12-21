e46e595efff87cf86fb14e92e282efe4684600d8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-21 16:14:38 -0500

    day30: Custom Class

--- src/day30_CustomClass/StudentObjects.java
+++ src/day30_CustomClass/StudentObjects.java
@@ -1,5 +1,7 @@
 package day30_CustomClass;
 
+import java.util.ArrayList;
+
 public class StudentObjects {
 
     public static void main(String[] args) {
@@ -18,6 +20,33 @@ public class StudentObjects {
 
         Student student5 = new Student();
         student5.setInfo("Mert", 'M', 26 ,2252, 'C');
+
+
+        Student[] students = {student1, student2, student3, student4, student5};
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+        System.out.println("-----------------------------------------------------");
+
+        ArrayList<Student> earlyBirds = new ArrayList<>(); // grade: A
+        ArrayList<Student> angryBirds = new ArrayList<>();
+
+        for (Student student : students) {
+
+            if(student.grade == 'A'){
+                earlyBirds.add(student);
+            }else{
+                angryBirds.add(student);
+            }
+
+        }
+
+        System.out.println(earlyBirds);
+
+        System.out.println(angryBirds);
+
 
     }
 
