3937893e8119909e2dfaf2def9edb248b2d3db2a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-21 10:12:19 -0500

    day30: Custom Class

---
+++ src/day30_CustomClass/WarmupTasks.java
@@ -0,0 +1,43 @@
+package day30_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class WarmupTasks {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(1,2,3,4,5,6,7,8));
+
+        Collections.swap(numbers, 0,  numbers.size()-1);
+
+        System.out.println(numbers);
+
+
+
+    }
+
+}
+/*
+1. write a program that can swap the first and last elements of an ArrayList
+
+2. Write a program that can move all the zeros to the last indexes of ArrayList
+
+					Ex:
+						list: {1,0,2,0,3,0,4,0}
+
+					output:
+						[1, 2, 3, 4, 0, 0, 0, 0]
+
+
+3. Write a program that can extract the special characters, digits and letters from a string and stores them into separate ArrayLists of Characters
+				Ex:
+					str = "ABCD123$%#@&456EFG!"
+
+				output:
+					list1: {1, 2, 3, 4, 5, 6}
+					list2: {A, B, C, D, E, F, G}
+					list3: {$, %, #, @, &, !}
+ */
