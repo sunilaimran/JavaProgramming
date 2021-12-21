160693ac071d1fde99fc4f261db6f739211a6825
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-21 17:02:02 -0500

    day30: Custom Class

--- src/day30_CustomClass/CapitalOne.java
+++ src/day30_CustomClass/CapitalOne.java
@@ -19,6 +19,34 @@ public class CapitalOne {
 
         Employee[] employees = { employee1, employee2, employee3, employee4, employee5 };
 
+        int countFullTime = 0;
+        int countPartTime = 0;
+        double max = employees[0].salary;
+        double min = employees[0].salary;
+
+        for (Employee employee : employees) {
+
+            if(employee.isFullTime){
+                countFullTime++;
+            }else{
+                countPartTime++;
+            }
+
+            if(employee.salary > max){
+                max = employee.salary;
+            }
+
+            if(employee.salary < min){
+                min = employee.salary;
+            }
+
+        }
+
+
+        System.out.println("countPartTime = " + countPartTime);
+        System.out.println("countFullTime = " + countFullTime);
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
 
 
 
