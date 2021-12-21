dc28e2b4a1e1b89133063c485817bba72a30b150
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-21 16:06:26 -0500

    day30: Custom Class

---
+++ src/day30_CustomClass/StudentObjects.java
@@ -0,0 +1,25 @@
+package day30_CustomClass;
+
+public class StudentObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.setInfo("Aygun", 'F', 39, 2210, 'A');
+
+        Student student2 = new Student();
+        student2.setInfo("Hulya", 'F', 26, 2123, 'B');
+
+        Student student3 = new Student();
+        student3.setInfo("Elminur", 'F',26,2215,'A');
+
+        Student student4 = new Student();
+        student4.setInfo("Ali", 'M',28,5511,'B');
+
+        Student student5 = new Student();
+        student5.setInfo("Mert", 'M', 26 ,2252, 'C');
+
+    }
+
+
+}
