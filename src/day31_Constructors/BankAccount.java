2c3284eff342e76ff8aed2070c2c436463f1ed9e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-27 11:45:47 -0500

    day31: Custom Class - Constructor

---
+++ src/day31_Constructors/BankAccount.java
@@ -0,0 +1,38 @@
+package day31_Constructors;
+
+public class BankAccount {
+
+    public String accountHolder;
+    public long accountNumber;
+    public double balance;
+
+    public void setInfo(String accountHolder, long accountNumber){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+
+    public String toString() {
+        return "BankAccount{" +
+                "accountHolder='" + accountHolder + '\'' +
+                ", accountNumber=" + accountNumber +
+                ", balance=" + balance +
+                '}';
+    }
+
+
+
+}
+
+/*
+BankAccount Task:
+	        Attributes:
+	                1. accountHolder, 2. accountNumber, 3. balance
+
+	        Actions:
+	        	1. setInfo(): sets the accountHolder and accountNumber attributes
+	        	2. toString()
+	            3. checkBalance():displays the available balance
+	            2. deposit(): increases the balance by the given amount
+				3. withdraw(): decreases the balance by the given amount
+
+ */
