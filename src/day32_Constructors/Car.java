2520b79641b75dd8b8e0a8ed18e5c2d747aa9c36
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-28 12:35:39 -0500

    day32: Custom Class - Constructor

---
+++ src/day32_Constructors/Car.java
@@ -0,0 +1,31 @@
+package day32_Constructors;
+
+public class Car {
+
+
+
+
+}
+
+/*
+class name: Car
+		 instance variables:
+		 		make, model, year, price, color
+
+ 		1st constructor: initializes the make ONLY
+
+ 		2nd constructor: initializes make & model
+ 				(MUST use constructor call to set the make)
+
+		3rd constructor: initializes make, model, year
+				(MUST use constructor call to set the make, model)
+
+		4th constructor: initializes make, model, year, price
+				(MUST use constructor call to set the make, model, year)
+
+		5th Constructor: intializes all the instances
+				(MUST use constructor call to set the make, mode, year, price, color)
+
+		instance methods: toString
+
+ */
