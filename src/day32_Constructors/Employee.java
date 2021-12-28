23a1010cf84904573080b5680fd01c98ba365f0b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-28 10:43:43 -0500

    day31: Custom Class - Constructor

---
+++ src/day32_Constructors/Employee.java
@@ -0,0 +1,17 @@
+package day32_Constructors;
+
+public class Employee {
+
+    public String name;
+    public char gender;
+    public String jobTitle;
+    public double salary;
+
+
+    public Employee(String name){
+        this.name = name;
+    }
+
+
+
+}
