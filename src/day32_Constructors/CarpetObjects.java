37884354c1bef64c5ddcf483f4ec000986026865
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-28 10:39:24 -0500

    day31: Custom Class - Constructor

--- src/day32_Constructors/CarpetObjects.java
+++ src/day32_Constructors/CarpetObjects.java
@@ -5,6 +5,7 @@ public class CarpetObjects {
     public static void main(String[] args) {
 
         Carpet carpet1 = new Carpet(3, 5, 8, true);
+
         Carpet carpet2 = new Carpet(10, 20, 10, false);
 
 
