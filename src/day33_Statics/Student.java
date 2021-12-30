903589214f8b0e72bd9888f70db9e2112b7dc97b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-30 10:24:35 -0500

    day33: Custom Class - Static

---
+++ src/day33_Statics/Student.java
@@ -0,0 +1,48 @@
+package day33_Statics;
+
+public class Student {
+
+    public String name;
+    public char gender;
+    public int age;
+    public int studentID;
+    public char grade;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, char gender) {
+        this(name);
+        this.gender = gender;
+    }
+
+
+
+
+
+}
+/*
+Student Task:
+		1. Create a class called Student:
+				Attributes:
+					name, gender, age, studentID, grade
+
+				Add a constructor that allows user to create object by setting the name of student
+
+				Add a constructor that allows user to create object by setting the name and gender of the student
+
+				Add a constructor that allows user to create object by setting the name and studentID of the student
+
+				Add a constructor that allows user to create object by setting the name, studentID and grade of the student
+
+				Add a constructor that allows user to create object by setting the name, gender and age of the student
+
+				Add a constructor that allows user to create object by setting the name, gende, age and studentID of the student
+
+				Add a constructor that allows user to create object by setting the name, gende, age, studentID and grade of the student
+
+				Actions:
+					toString()
+					study()
+ */
