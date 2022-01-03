0d4866fcf9a19241a9129510f7d082ecf074fa04
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-03 15:59:00 -0500

    day34: Garbage Collection & Access Modifiers

--- src/day34_GarbageCollection_AccessModifiers/AccessModifiers_Test.java
+++ src/day34_GarbageCollection_AccessModifiers/AccessModifiers_Test.java
@@ -1,4 +1,14 @@
 package day34_GarbageCollection_AccessModifiers;
 
 public class AccessModifiers_Test {
+
+    public static void main(String[] args) {
+
+        System.out.println( AccessModifiers.publicData );
+        System.out.println( AccessModifiers.defaultData );
+    //    System.out.println( AccessModifiers.privateData ); // private is not visible outside the class
+
+
+    }
+
 }
