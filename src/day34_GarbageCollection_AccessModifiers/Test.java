0e02b2260def84404623f1ad1656ff7dcfecb9b7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-03 12:44:40 -0500

    day34: Custom Class - Static

--- src/day34_GarbageCollection_AccessModifiers/Test.java
+++ src/day34_GarbageCollection_AccessModifiers/Test.java
@@ -1,12 +1,14 @@
 package day34_GarbageCollection_AccessModifiers;
 
+import static day34_GarbageCollection_AccessModifiers.Circle.*;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println(Circle.pi);
-        System.out.println(Circle.name);
-        System.out.println(Circle.numbers);
+        System.out.println(pi);
+        System.out.println(name);
+        System.out.println(numbers);
 
 
     }
