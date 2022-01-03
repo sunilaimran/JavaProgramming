0a1d60afcdd1977fc2a1b711378c9aa5d0a388f5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-03 12:53:44 -0500

    day34: Custom Class - Static

--- src/day34_GarbageCollection_AccessModifiers/Test.java
+++ src/day34_GarbageCollection_AccessModifiers/Test.java
@@ -1,6 +1,9 @@
 package day34_GarbageCollection_AccessModifiers;
 
+
 import static day34_GarbageCollection_AccessModifiers.Circle.*;
+
+import static utilities.MathUtility.*;
 
 public class Test {
 
@@ -9,6 +12,19 @@ public class Test {
         System.out.println(pi);
         System.out.println(name);
         System.out.println(numbers);
+
+        System.out.println("----------------------------");
+
+        // find the sum of 10, 20
+        int r1 = sum(10, 20);
+
+        // find the sum of 100, 200
+        int r2 = sum(100, 200);
+
+
+        int r3 = subtract(100,50);
+
+        int r4 = max(1000, 2000);
 
 
     }
