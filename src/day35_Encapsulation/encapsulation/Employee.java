3e0eeaf1771b2457857a8eafa31fa274fec32f8d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-04 12:37:51 -0500

    day35_Encapsulation

---
+++ src/day35_Encapsulation/encapsulation/Employee.java
@@ -0,0 +1,63 @@
+package day35_Encapsulation.encapsulation;
+
+public class Employee {
+
+    private String name;
+    private char gender;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+
+    public void setName(String name) {
+        if(name.isEmpty()){
+            return;
+        }
+        this.name = name;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+}
+
+/*
+1. Create an Employee Class:
+			private variables:
+				name, gender, age, salary
+
+			Encapsulate all the private fields
+
+				1. Name can not be empty
+				2. gender can not anything other than female or male
+				3. age can not be less than 16, and greater than 90
+				4. salary can not be zero or negative
+
+
+
+ */
