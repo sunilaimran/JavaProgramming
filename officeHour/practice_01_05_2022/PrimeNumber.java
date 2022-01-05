0367fca695e10f0b30921961caa2a91493dc742e
Aysun120 <aysundamar114@gmail.com>
2022-01-05 16:16:43 -0500

    RemoveSpaces added

--- officeHour/practice_01_05_2022/PrimeNumber.java
+++ officeHour/practice_01_05_2022/PrimeNumber.java
@@ -17,7 +17,7 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
 
-        for(int i=0;i<1000;i++){
+        for(int i=0;i<100;i++){
             if(isPrime(i)){
                 System.out.print(i+" ");
             }
