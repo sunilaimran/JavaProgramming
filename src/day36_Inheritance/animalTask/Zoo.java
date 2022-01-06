e58536620c7d58e01734a703c0c637310ab4a678
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-06 13:04:42 -0500

    day36: Inheritance Intro

--- src/day36_Inheritance/animalTask/Zoo.java
+++ src/day36_Inheritance/animalTask/Zoo.java
@@ -5,10 +5,22 @@ public class Zoo {
     public static void main(String[] args) {
 
         Dog dog = new Dog();
+        dog.setInfo("Max", "Husky", 'M', 2, "Small", "White");
+
+        dog.eat();
+        dog.drink();
+        dog.sleep();
+        dog.move();
+        dog.bark();
+       // dog.hunt();
+       // dog.scratch();
 
         Cat cat = new Cat();
+        cat.setInfo("Tarcin","British",'M',3,"Small","brown");
+
 
         Tiger tiger = new Tiger();
+        tiger.setInfo("Sher Khan", "Bengal", 'M', 4, "Large", "Red");
 
 
     }
