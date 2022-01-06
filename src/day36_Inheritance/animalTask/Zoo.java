3817a440de0f6397305a1e5c97373731f941476d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-06 12:58:58 -0500

    day36: Inheritance Intro

---
+++ src/day36_Inheritance/animalTask/Zoo.java
@@ -0,0 +1,16 @@
+package day36_Inheritance.animalTask;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        Cat cat = new Cat();
+
+        Tiger tiger = new Tiger();
+
+
+    }
+
+}
