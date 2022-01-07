3eb19c424098360147e8a5ea3350b925d979c8cd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 15:51:38 -0500

    day37: Inheritance Continue

---
+++ src/day37_Inheritance/scrumTask/Person.java
@@ -0,0 +1,37 @@
+package day37_Inheritance.scrumTask;
+
+public class Person {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public void eat(String food){
+        System.out.println(name + " is eating");
+    }
+
+    public void drink(String drink){
+        System.out.println(name + " is drinking");
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+
+}
+
+/*
+Create a class named Person
+            variables:
+                name, age, gender
+
+            methods:
+                eat(String food)
+                drink(String drink)
+                toString()
+ */
