26765dbdca46bfd5a4410eba2929af365268a336
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 16:23:13 -0500

    day37: Inheritance Continue

---
+++ src/day37_Inheritance/scrumTask/BusinessAnalyst.java
@@ -0,0 +1,11 @@
+package day37_Inheritance.scrumTask;
+
+public class BusinessAnalyst extends Employee {
+
+
+    public BusinessAnalyst(String name, int age, char gender, String jobTitle, int id, double salary, String companyName) {
+        super(name, age, gender, jobTitle, id, salary, companyName);
+    }
+
+
+}
