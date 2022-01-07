420c054a344d5883341a7b2baa1314185ca4debf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 17:01:17 -0500

    day37: Inheritance Continue

---
+++ src/day37_Inheritance/scrumTask/AmazonInc.java
@@ -0,0 +1,32 @@
+package day37_Inheritance.scrumTask;
+
+public class AmazonInc {
+
+    public static void main(String[] args) {
+
+        String company = "Amazon Inc";
+
+        ProductOwner po = new ProductOwner("Suhaib", 29, 'M',1,160000, company);
+
+        BusinessAnalyst ba = new BusinessAnalyst("Cihad", 30, 'M', 2, 150000, company);
+
+        ScrumMaster sm = new ScrumMaster("Anel", 26, 'F', 3, 145000, company);
+
+
+
+        Tester tester1=new Tester("Chinara",32,'F',"QA",1415,125000,company);
+
+        Tester tester2=new Tester("Yasaman",31,'F', "QE",1416,130000,company);
+
+        Tester tester3=new Tester("Irena",29,'F',"SDET",1417,128000,company);
+
+        Tester tester4=new Tester("Cihad",26,'M',"QA",1418,125000,company);
+        Tester[] testers={tester1,tester2,tester3,tester4};
+
+
+
+
+
+    }
+
+}
