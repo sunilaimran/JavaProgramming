e49a7e9108ec7b10918c9d17f9c6d7bd415fb4af
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 11:10:15 -0500

    day37: Inheritance Continue

--- src/day37_Inheritance/animalTask/Parrot.java
+++ src/day37_Inheritance/animalTask/Parrot.java
@@ -2,8 +2,11 @@ package day37_Inheritance.animalTask;
 
 public class Parrot extends Animal {
 
+   // public String parrotWingColor;
+
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
+       // this.parrotWingColor = parrotWingColor;
     }
 
     public void sing(){
