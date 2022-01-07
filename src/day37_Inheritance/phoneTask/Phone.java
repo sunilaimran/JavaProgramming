92f9f324ecb0b9c4d331957df12f830913083e35
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 14:38:42 -0500

    day37: Inheritance Continue

---
+++ src/day37_Inheritance/phoneTask/Phone.java
@@ -0,0 +1,25 @@
+package day37_Inheritance.phoneTask;
+
+public class Phone {
+
+    public String brand;
+    public String model;
+    public String size;
+    public double price;
+    public String color;
+
+    public static boolean hasBattery = true;
+
+    public Phone(String brand, String model, String size, double price, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.size = size;
+        this.price = price;
+        this.color = color;
+    }
+
+
+
+
+
+}
