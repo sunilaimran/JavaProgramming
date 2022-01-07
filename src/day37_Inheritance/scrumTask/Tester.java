8c579efae820a545455b2e1dd7ecce2aa6093f44
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-07 16:19:23 -0500

    day37: Inheritance Continue

--- src/day37_Inheritance/scrumTask/Tester.java
+++ src/day37_Inheritance/scrumTask/Tester.java
@@ -1,6 +1,6 @@
 package day37_Inheritance.scrumTask;
 
-public class Tester extends Employee{
+public class Tester extends Employee{ //Tester IS A Employee,  Tester IS A Person
 
     public Tester(String name, int age, char gender, String jobTitle, int id, double salary, String companyName) {
         super(name, age, gender, jobTitle, id, salary, companyName);
